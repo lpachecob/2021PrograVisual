57dc7daecf16d76338339ad89ad18da5147668d2
Luis Pacheco <lpachecoby@gmail.com>
2021-09-30 16:15:43 -0500

    Registros

---
+++ PrograVisual/Vista/ProductosDataGridView.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace PrograVisual.Vista
+{
+    public partial class ProductosDataGridView : PrograVisual.Vista.Layout
+    {
+        public ProductosDataGridView()
+        {
+            InitializeComponent();
+        }
+    }
+}
